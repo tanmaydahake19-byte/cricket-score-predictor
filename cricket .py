866481d114a6f335c393d9edbb5c8fb6f1aea83e
tanmaydahake19-byte <tanmaydahake19@gmail.com>
2026-07-20 18:19:59 +0530

    Add files via upload

---
+++ cricket .py
@@ -0,0 +1,9 @@
+Runs=int(input("Enter the number of runs scored: "))
+overs=float(input("enter the overs bowled:"))
+overs_left=20-overs
+balls_left=overs_left*6
+print("Overs left:", overs_left)
+print("Balls left:", balls_left)
+
+projected_score=(Runs/overs)*overs_left
+print("Projected runs:", projected_score)
